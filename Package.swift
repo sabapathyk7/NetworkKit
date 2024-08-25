5a185012532b1e8a5a23b2ddc78add6f4a2c6933
Kanagasabapathy <trksabapathy7@gmail.com>
2024-08-26 00:48:09 +0530

    Added swiftSettings to check the complete concurrency - StrictConcurrency by using enableUpcomingFeature - Swift 6

--- Package.swift
+++ Package.swift
@@ -20,7 +20,10 @@ let package = Package(
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "NetworkKit"),
+            name: "NetworkKit",
+            swiftSettings: [
+            .enableUpcomingFeature("StrictConcurrency"),
+        ]),
         .testTarget(
             name: "NetworkKitTests",
             dependencies: ["NetworkKit"])
